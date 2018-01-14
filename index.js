6ad75e14ffe1f4f693933ee7a6f23710788669f5
Amanda Bromberg <7b4571c5@opayq.com>
2018-01-14 04:21:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
+//ONE
 function shout(string)
 {
 return string.toUpperCase()
 }
 
+//TWO
 function whisper(string)
 {
   return string.toLowerCase()
 }
 
+//THREE
 function logShout(string)
 {
   console.log("HELLO")
 }
 
+//FOUR
 function logWhisper(string)
 {
   console.log(string.toLowerCase)
